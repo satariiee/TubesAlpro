69e23266e1dadee3559ed092701821d8bd67cb8b
mhmdmulna <mhmdmulna14@gmail.com>
2025-05-17 17:20:00 +0700

    Update main.go

--- main.go
+++ main.go
@@ -4,6 +4,24 @@ import (
 	"fmt"
 )
 
+const NMAX int = 1000
+
+type sampah struct {
+	jenis           string
+	jumlah          int
+	daurUlang       bool
+	metodeDaurUlang string
+}
+
+var dataSampah [NMAX]sampah
+
 func main() {
-	fmt.Print("Hello World")
+	fmt.Println("\n--- Aplikasi Pengelolaan Sampah ---")
+	fmt.Println("1. Tambah Data Sampah")
+	fmt.Println("2. Ubah Data Sampah")
+	fmt.Println("3. Hapus Data Sampah")
+	fmt.Println("4. Cari Sampah")
+	fmt.Println("5. Urutkan Sampah")
+	fmt.Println("6. Tampilkan Statistik")
+	fmt.Println("0. Keluar")
 }
