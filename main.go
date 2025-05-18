8b37187984c55b1c967f264f362753eee14f566b
mhmdmulna <mhmdmulna14@gmail.com>
2025-05-18 14:36:59 +0700

    Update main.go

--- main.go
+++ main.go
@@ -13,9 +13,10 @@ type sampah struct {
 	metodeDaurUlang string
 }
 
-var dataSampah [NMAX]sampah
+type dataSampah [NMAX]sampah
 
 func main() {
+	var data dataSampah
 	fmt.Println("\n--- Aplikasi Pengelolaan Sampah ---")
 	fmt.Println("1. Tambah Data Sampah")
 	fmt.Println("2. Ubah Data Sampah")
@@ -24,4 +25,16 @@ func main() {
 	fmt.Println("5. Urutkan Sampah")
 	fmt.Println("6. Tampilkan Statistik")
 	fmt.Println("0. Keluar")
+
+	var pilihan int
+	fmt.Scan(&pilihan)
+
+	switch pilihan {
+	case 1:
+		tambahData(&data)
+	}
+}
+
+func tambahData(A *dataSampah) {
+	fmt.Scan(A)
 }
